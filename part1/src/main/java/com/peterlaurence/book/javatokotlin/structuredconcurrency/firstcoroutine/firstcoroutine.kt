af97d62924ed4cf39e0e000d5fcfa43edc7f09e7
peterLaurence <peter.laurence@protonmail.com>
2019-12-28 15:10:25 +0100

    Coroutine sample

--- part1/src/main/java/com/peterlaurence/book/javatokotlin/structuredconcurrency/firstcoroutine/firstcoroutine.kt
+++ part1/src/main/java/com/peterlaurence/book/javatokotlin/structuredconcurrency/firstcoroutine/firstcoroutine.kt
@@ -1,15 +1,11 @@
 package com.peterlaurence.book.javatokotlin.structuredconcurrency.firstcoroutine
 
 import kotlinx.coroutines.*
-import kotlin.coroutines.ContinuationInterceptor
 
-@InternalCoroutinesApi
 fun main() = runBlocking {
     val job = launch {
         while (true) {
             println("I'm working")
-            val x = coroutineContext[ContinuationInterceptor] as? Delay
-            println(x)
             delay(10)
         }
     }
