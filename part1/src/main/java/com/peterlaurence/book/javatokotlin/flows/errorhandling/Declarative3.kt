b4569a53bd38c07ec211019531f29e6d0ed3159c
plr <plr.devs@gmail.com>
2020-06-23 17:34:29 +0200

    Add emit from catch example

---
+++ part1/src/main/java/com/peterlaurence/book/javatokotlin/flows/errorhandling/Declarative3.kt
@@ -0,0 +1,23 @@
+package com.peterlaurence.book.javatokotlin.flows.errorhandling.declarative3
+
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+val upstream = flowOf(1, 3, -1)
+
+val encapsulateError = upstream
+    .onEach {
+        if (it < 0) throw NumberFormatException("Values should be greater than 0")
+    }
+    .catch { e ->
+        emit(0)
+    }
+
+fun main() = runBlocking {
+    encapsulateError.collect {
+        println("Received $it")
+    }
+}
