e1064200002b04e20eaeaed63be00b9493d9f4e8
plr <plr.devs@gmail.com>
2020-09-06 09:47:25 +0200

    Kotlin 1.4

--- part1/src/main/java/com/peterlaurence/book/javatokotlin/callbackpattern/fragments/PurchasesFragment.kt
+++ part1/src/main/java/com/peterlaurence/book/javatokotlin/callbackpattern/fragments/PurchasesFragment.kt
@@ -2,7 +2,7 @@ package com.peterlaurence.book.javatokotlin.callbackpattern.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 
 class PurchasesFragment : Fragment() {
@@ -12,16 +12,10 @@ class PurchasesFragment : Fragment() {
         /* Create a ViewModel the first time this Fragment is created.
          * Re-created Fragment receives the same ViewModel instance after device rotation. */
         val factory: ViewModelProvider.Factory = PurchaseViewModelFactory()
-        val model =
-            ViewModelProvider(this, factory).get(
-                PurchasesViewModel::class.java
-            )
-        model.purchasesLiveData.observe(
-            this,
-            Observer { (_, purchases) ->
-                // update UI
-                println(purchases)
-            }
-        )
+        val model by viewModels<PurchasesViewModel> { factory }
+        model.purchasesLiveData.observe(this) { (_, purchases) ->
+            // update UI
+            println(purchases)
+        }
     }
 }
