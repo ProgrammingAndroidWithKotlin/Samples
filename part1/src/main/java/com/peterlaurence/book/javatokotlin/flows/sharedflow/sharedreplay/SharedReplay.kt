5abba7729a1e558d946d7d4b13ba22782aaec00b
plr <plr.devs@gmail.com>
2020-11-12 19:22:49 +0100

    StateFlow example

--- part1/src/main/java/com/peterlaurence/book/javatokotlin/flows/sharedflow/sharedreplay/SharedReplay.kt
+++ part1/src/main/java/com/peterlaurence/book/javatokotlin/flows/sharedflow/sharedreplay/SharedReplay.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.collect
 
 class NewsRepository(private val dao: NewsDao) {
-    private val _newsFeed = MutableSharedFlow<News>(0)
+    private val _newsFeed = MutableSharedFlow<News>()
     val newsFeed = _newsFeed.asSharedFlow()
 
     private val scope = CoroutineScope(Job() + Dispatchers.IO)
@@ -45,6 +45,28 @@ class NewsViewsModel(private val repository: NewsRepository) {
     }
 }
 
+/**
+ * This is the version you would use in a real Android application
+ */
+//class NewsViewsModel @ViewModelInject constructor(
+//    private val repository: NewsRepository
+//) : ViewModel() {
+//    private val newsList = mutableListOf<News>()
+//
+//    private val _newsLiveData = MutableLiveData<List<News>>(newsList)
+//    val newsLiveData: LiveData<List<News>> = _newsLiveData
+//
+//    init {
+//        viewModelScope.launch {
+//            repository.newsFeed.collect {
+//                println("NewsViewsModel receives $it")
+//                newsList.add(it)
+//                _newsLiveData.value = newsList
+//            }
+//        }
+//    }
+//}
+
 class AnotherViewModel(private val repository: NewsRepository) {
     private val viewModelScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
@@ -63,13 +85,13 @@ fun main() = runBlocking {
         private var index = 0
 
         override suspend fun fetchNewsFromApi(): List<News> {
-            delay(1000)
+            delay(100)
             return listOf(News("news content ${++index}"), News("news content ${++index}"))
         }
     }
     val repo = NewsRepository(dao)
     NewsViewsModel(repo)
-    delay(5000)
+    delay(150)
     AnotherViewModel(repo)
 
     delay(30_000)
--- part1/src/main/java/com/peterlaurence/book/javatokotlin/flows/sharedflow/stateflow/stateflow.kt
+++ part1/src/main/java/com/peterlaurence/book/javatokotlin/flows/sharedflow/stateflow/stateflow.kt
@@ -1,8 +1,82 @@
 package com.peterlaurence.book.javatokotlin.flows.sharedflow.stateflow
 
-import java.io.File
+import android.app.Service
+import android.content.Intent
+import android.os.Bundle
+import android.os.IBinder
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-sealed class DownloadEvent
-data class DownloadProgress(val progress: Int): DownloadEvent()
-data class DownloadComplete(val file: File): DownloadEvent()
-object DownloadError: DownloadEvent()
+sealed class ServiceStatus
+object Started : ServiceStatus()
+data class Aborted(val reason: String): ServiceStatus()
+object Stopped : ServiceStatus()
+
+class DownloadService : Service() {
+    companion object {
+        private val _downloadState = MutableStateFlow<ServiceStatus>(Stopped)
+        val downloadState = _downloadState.asStateFlow()
+    }
+
+
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+
+    override fun onBind(intent: Intent?): IBinder? {
+        // We don't provide binding, so return null
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val url = intent.getStringExtra("url")
+        if (url == null) {
+            stopService()
+            return START_NOT_STICKY
+        }
+
+        _downloadState.value = Started
+        scope.launch {
+            downloadUrl(url)
+            stopService()
+        }
+        return START_NOT_STICKY
+    }
+
+    private suspend fun downloadUrl(url: String) {
+        /* Simulate a download */
+        delay(1000)
+    }
+
+    private fun stopService() {
+        _downloadState.tryEmit(Stopped)
+        stopSelf()
+    }
+}
+
+class DownloadViewModel : ViewModel() {
+    val downloadServiceStatus = DownloadService.downloadState.asLiveData()
+}
+
+class DownloadFragment : Fragment() {
+    private val viewModel: DownloadViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel.downloadServiceStatus.observe(this) {
+            it?.also {
+                onDownloadServiceStatus(it)
+            }
+        }
+    }
+
+    private fun onDownloadServiceStatus(status: ServiceStatus): Nothing = when(status) {
+        Started -> TODO()
+        Stopped -> TODO()
+        is Aborted -> TODO()
+    }
+}
